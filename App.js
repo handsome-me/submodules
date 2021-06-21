f0cd58b97814d50e3e5bec1bd8ad00912d24007d
Meghraj Deshmukh <meghraj.deshmukh103@gmail.com>
2021-06-21 21:12:41 +0530

    add changes to submodules

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export  function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>This screen is inside subm!</Text>
+     
     </View>
   );
 }
 
+export const ShareScreen=()=>{
+ return (
+    <View style={styles.container}>
+      <Text style={styles.container}>This alignItems submodules ......!</Text>
+     
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color:'green'
   },
+ 
 });
